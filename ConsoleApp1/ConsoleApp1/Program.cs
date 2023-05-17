81ed7f0228ab66532cff071e73923fe0fd1ed955
DmitriyLosev8 <dmitriylosev8@gmail.com>
2023-05-17 22:12:15 +0300

    Оружие

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Napilnik1
+{
+    class Weapon
+    {
+        private int _damage;
+        private int _bullets;
+       
+        public void Fire(Player player)
+        {
+            player.TakeDamage(_damage);
+            _bullets -= 1;
+        }
+    }
+
+    class Player
+    {
+        private int _health;
+
+        public void TakeDamage(int damage)
+        {
+            _health -= damage;
+        }
+    }
+
+    class Bot
+    {
+        private Weapon _weapon;
+        private Player _player;
+
+        public Bot(Player player, Weapon weapon)
+        {
+            _player = player;
+            _weapon = weapon;
+        }
+
+        public void OnSeePlayer()
+        {
+            _weapon.Fire(_player);
+        }
+    }
+
+}
+

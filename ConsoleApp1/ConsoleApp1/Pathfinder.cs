4717054870722188e00fc1092b843157477c5da1
DmitriyLosev8 <dmitriylosev8@gmail.com>
2023-05-20 17:48:06 +0300

    Логирование

---
+++ ConsoleApp1/ConsoleApp1/Pathfinder.cs
@@ -0,0 +1,12 @@
+﻿namespace Napilnik1
+{
+    internal class PathFinder
+    {
+        private int v;
+
+        public PathFinder(int v)
+        {
+            this.v = v;
+        }
+    }
+}
--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,44 +1,102 @@
 ﻿using System;
+using System.IO;
 
 namespace Napilnik1
 {
-    class Weapon
+    class Program
     {
-        private int _damage;
-        private int _bullets;
-       
-        public void Fire(Player player)
+        static void Main(string[] args)
         {
-            player.TakeDamage(_damage);
-            _bullets -= 1;
-        }
-    }
-
-    class Player
-    {
-        private int _health;
-
-        public void TakeDamage(int damage)
-        {
-            _health -= damage;
-        }
-    }
-
-    class Bot
-    {
-        private Weapon _weapon;
-        private Player _player;
-
-        public Bot(Player player, Weapon weapon)
-        {
-            _player = player;
-            _weapon = weapon;
+            Pathfinder variantOfLogger1 = new Pathfinder(1);
+            Pathfinder variantOfLogger2 = new Pathfinder(2);
+            Pathfinder variantOfLogger3 = new Pathfinder(3);
+            Pathfinder variantOfLogger4 = new Pathfinder(4);
+            Pathfinder variantOfLogger5 = new Pathfinder(5);
         }
 
-        public void OnSeePlayer()
+        class Writer
         {
-            _weapon.Fire(_player);
+            public virtual void WriteError(string message) {}
+        }
+
+        class ConsoleLogWritter : Writer
+        {
+            public override void WriteError(string message)
+            {
+                Console.WriteLine(message);
+            }
+        }
+
+        class FileLogWritter : Writer
+        {
+            public override void WriteError(string message)
+            {
+                File.WriteAllText("log.txt", message);
+            }
+        }
+
+        class SecureLogWritter 
+        { 
+            public void SecuredWrite(Writer writer, string message)
+            {
+                if (DateTime.Now.DayOfWeek == DayOfWeek.Friday)
+                {
+                    writer.WriteError(message);
+                }
+            }
+        }
+
+        class Pathfinder : Logger
+        {
+            public Pathfinder(int variantOfLogger) : base(variantOfLogger) {}
+           
+            public void Find(string message)
+            {
+                ChooseLoggerToWrite(message);
+            }
+        }
+
+        class Logger
+        {
+            private int _variantOfLogger;
+            private ConsoleLogWritter _consoleLogWritter = new ConsoleLogWritter();
+            private FileLogWritter _fileLogWritter = new FileLogWritter();
+            private SecureLogWritter _secureLogWritter = new SecureLogWritter();
+
+            public Logger(int variantOfLogger)
+            {
+                _variantOfLogger = variantOfLogger;
+            }
+
+            protected void ChooseLoggerToWrite(string message)
+            {
+                switch (_variantOfLogger)
+                {
+                    case 1:
+                        _fileLogWritter.WriteError(message);
+                        break;
+                    case 2:
+                        _consoleLogWritter.WriteError(message);
+                        break;
+                    case 3:
+                        _secureLogWritter.SecuredWrite(_fileLogWritter,message);
+                        break;
+                    case 4:
+                        _secureLogWritter.SecuredWrite(_consoleLogWritter, message);
+                        break;
+                    case 5:
+                        _consoleLogWritter.WriteError(message);
+                        _secureLogWritter.SecuredWrite(_fileLogWritter, message);
+                        break;
+                }
+            }
         }
     }
 }
+    
+
+
+
+
+
 
